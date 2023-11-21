46d66a77bbe7a295d8fd7223c6ffca9ab1a7e5d6
brookewil <bwilson4@ithaca.edu>
2023-11-21 16:21:49 -0500

    changed "artists" to "allArtists" for clarification + began adding artist/<name> route

--- app/routes.py
+++ app/routes.py
@@ -11,20 +11,16 @@ from app.models import *
 def index():
     return render_template('index.html', title='Ithaca Music')
 
-@app.route('/artists', methods=['GET', 'POST'])
-def artists():
+@app.route('/allArtists', methods=['GET', 'POST'])
+def allArtists():
     artists_list = db.session.query(Artist).all()
-    return render_template('artists.html', title="List of Artists", artists_list=artists_list)
+    return render_template('allArtists.html', title="List of Artists", artists_list=artists_list)
 
-@app.route('/artist/<name>', methods=['GET', 'POST'])
-def gunpoets():
-    details = {
-        "name": "The Gunpoets",
-        "hometown": "Ithaca",
-        "description": "Voice as a weapon, words as bullets, spreading the universal message of peace, love, and justice through music. Sure, there's a cynical cultural tendency to make certain assumptions when you hear the word \"gun\" associated with rap music, but this seven-member live hip-hop band from Ithaca, NY, runs contrary to that image with their positive message and uplifting performances.",
-        "events": ["The Commons on Thursday 9/6", "The Haunt next Friday 9/14"]
-    }
-    return render_template('gunpoets.html',  title='Gunpoets', details=details)
+@app.route('/allArtist/<name>')
+def artist(name):
+    artist = db.session.query(Artist).filter_by(name=name)
+
+    return render_template('artist.html', title=name, artist=artist)
 
 @app.route('/newartists', methods=['GET', 'POST'])
 def newartists():
