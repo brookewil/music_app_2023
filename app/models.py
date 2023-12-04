78a8b478db1a103ad78ca89ea82f45f87c2f371c
brookewil <bwilson4@ithaca.edu>
2023-12-03 19:56:06 -0500

    User Model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Artist(db.Model):
     __tablename__="Artist"
@@ -41,3 +43,21 @@ class ArtistToEvent(db.Model):
 
     def __repr__(self):
         return '<ArtistToEvent {}>'.format(self.id)
+
+class User(UserMixin, db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
